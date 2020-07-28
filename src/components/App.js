91aca1199310fda956ea50a92644e6d8efcc516a
KennethLloyd <verastiguelloyd@gmail.com>
2020-07-28 09:01:09 +0800

    Add auth and unauth routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,27 @@
 import React from 'react';
+import { Router, Switch } from 'react-router-dom';
 import { GlobalProvider } from '../context/GlobalState';
+import AuthenticatedRoute from './AuthenticatedRoute';
+import UnauthenticatedRoute from './UnauthenticatedRoute';
 import { UserProvider } from '../context/UserState';
 import Login from './auth/Login';
+import Dashboard from './dashboard/Dashboard';
 import './App.scss';
+import history from '../history.js';
 
 const App = () => {
   return (
     <GlobalProvider>
-      <UserProvider>
+      <Router history={history}>
+        <Switch>
+          <AuthenticatedRoute path="/" exact component={Dashboard} />
+          <UnauthenticatedRoute path="/login" exact component={Login} />
+          {/* <UnauthenticatedRoute path="/signup" exact component={UserSignUp} /> */}
+        </Switch>
+      </Router>
+      {/* <UserProvider>
         <Login />
-      </UserProvider>
+      </UserProvider> */}
     </GlobalProvider>
   );
 };
