b655605aa448bda6a6d76457d868dad020ee3026
KennethLloyd <verastiguelloyd@gmail.com>
2020-07-30 09:57:54 +0800

    Add dev ssl certificates

---
+++ src/components/auth/FacebookSignIn.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook } from '@fortawesome/free-brands-svg-icons';
+
+const FacebookSignIn = () => {
+  const responseFacebook = (response) => {
+    console.log(response);
+  };
+
+  return (
+    <FacebookLogin
+      appId={process.env.REACT_APP_FB_APP_ID}
+      autoLoad
+      callback={responseFacebook}
+      render={(renderProps) => (
+        <button className="fb-btn">
+          <FontAwesomeIcon icon={faFacebook} className="brand-icon" />
+          Connect with Facebook
+        </button>
+      )}
+    />
+  );
+};
+
+export default FacebookSignIn;
--- src/components/auth/SocialLogin.js
+++ src/components/auth/SocialLogin.js
@@ -1,17 +1,13 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook } from '@fortawesome/free-brands-svg-icons';
 import GoogleSignIn from './GoogleSignIn';
+import FacebookSignIn from './FacebookSignIn';
 
 const SocialLogin = () => {
   return (
     <div className="card-left">
       <h5>Via social networking accounts</h5>
       <GoogleSignIn />
-      <button className="fb-btn">
-        <FontAwesomeIcon icon={faFacebook} className="brand-icon" />
-        Connect with Facebook
-      </button>
+      <FacebookSignIn />
     </div>
   );
 };
