be790692895e985ab53910b8808253de0fea2535
KennethLloyd <verastiguelloyd@gmail.com>
2020-08-07 17:06:14 +0800

    Add propagate loader

--- src/components/UnauthenticatedRoute.js
+++ src/components/UnauthenticatedRoute.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import PropagateLoader from 'react-spinners/PropagateLoader';
 import { GlobalContext } from '../context/GlobalState';
 import { UserProvider } from '../context/UserState';
 
@@ -7,12 +8,25 @@ const UnauthenticatedRoute = ({ children, ...rest }) => {
   const { token } = useContext(GlobalContext);
 
   const isAuthenticated = token ? true : false;
+  const { isLoading } = useContext(GlobalContext);
 
   return (
     <UserProvider>
       <Route {...rest}>
         {!isAuthenticated ? children : <Redirect to="/" />}
       </Route>
+      <PropagateLoader
+        css={`
+          position: fixed;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          z-index: 2;
+        `}
+        color={'#50c878'}
+        size={20}
+        loading={isLoading}
+      />
     </UserProvider>
   );
 };
