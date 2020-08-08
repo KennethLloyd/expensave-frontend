af89c0cb553dd0e66be20d07330c3e4f8a2229e6
KennethLloyd <verastiguelloyd@gmail.com>
2020-08-08 08:52:44 +0800

    Add transaction header

--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { UserContext } from '../../context/UserState';
 import './Transactions.scss';
 import Header from '../Header';
+import TransactionHeader from './TransactionHeader';
 import SideNav from '../SideNav';
 
 const Transactions = () => {
@@ -10,6 +11,7 @@ const Transactions = () => {
   return (
     <div className="transaction-page">
       <Header />
+      <TransactionHeader />
       <SideNav />
       {/* <h1>Transactions</h1>
       <button
