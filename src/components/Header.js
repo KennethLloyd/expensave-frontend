5140b5d299919cfa0b86d0e43f62ed1e8eb77c3a
KennethLloyd <verastiguelloyd@gmail.com>
2020-08-13 20:32:46 +0800

    Header UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWallet } from '@fortawesome/free-solid-svg-icons';
 import './Header.scss';
 
 const Header = () => {
   return (
     <header>
       <h3>Expensave</h3>
-      <h4>Total</h4>
+      <div className="header-right">
+        <FontAwesomeIcon
+          icon={faWallet}
+          className="header-wallet-icon"
+          size="3x"
+          color="white"
+        />
+        &nbsp;&nbsp;
+        <div className="header-total">
+          <p>Total</p>
+          <h4>P 10,500.00</h4>
+        </div>
+      </div>
     </header>
   );
 };
