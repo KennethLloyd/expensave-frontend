0c3a17181b1705e2a894a2ba059c48a2c2c90677
KennethLloyd <verastiguelloyd@gmail.com>
2020-08-13 21:53:49 +0800

    Centralize home components

--- src/components/transactions/Transactions.js
+++ src/components/transactions/Transactions.js
@@ -1,26 +1,11 @@
-import React, { useContext } from 'react';
-import { UserContext } from '../../context/UserState';
+import React from 'react';
 import './Transactions.scss';
-import Header from '../Header';
 import TransactionHeader from './TransactionHeader';
-import SideNav from '../SideNav';
 
 const Transactions = () => {
-  const { logOut } = useContext(UserContext);
-
   return (
     <div className="transaction-page">
-      <Header />
       <TransactionHeader />
-      <SideNav />
-      {/* <h1>Transactions</h1>
-      <button
-        onClick={() => {
-          logOut();
-        }}
-      >
-        Logout
-      </button> */}
     </div>
   );
 };
