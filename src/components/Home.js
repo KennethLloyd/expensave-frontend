e7026d84c38cee6a36ae2f64afd569ccdef4eb3a
KennethLloyd <verastiguelloyd@gmail.com>
2020-12-23 15:38:45 +0800

    Add logout button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { Grid, AppBar, Toolbar, Typography } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Grid, AppBar, Toolbar, Typography, Button } from '@material-ui/core';
 import AddTransaction from './transaction/AddTransaction';
 import TransactionList from './transaction/TransactionList';
+import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
 const Home = () => {
   return (
@@ -15,6 +15,10 @@ const Home = () => {
               <Typography variant="h6">Expensave</Typography>
             </Grid>
           </Grid>
+          <Button size="small" style={{ color: 'white' }}>
+            <ExitToAppIcon />
+            &nbsp;Logout
+          </Button>
         </Toolbar>
       </AppBar>
       <Grid container spacing={2} justify="space-around" direction="row">
